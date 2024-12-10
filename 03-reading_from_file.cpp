b59ce8b325a36173a1def16edd197dc386b97f21
Gordon Novak <161096797+gordonovak@users.noreply.github.com>
2024-12-10 15:37:20 -0600

    Update 03-reading_from_file.cpp

--- 03-reading_from_file.cpp
+++ 03-reading_from_file.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// Example struct
 struct Person {
     string firstname;
     string lastname;
@@ -17,7 +18,7 @@ struct Person {
     }
 };
 
-// How to grab data from a text file with this format
+// This file covers how to use an ifstream object to grab data from a file
 
 // Lets say I have a person in my data.txt that looks like this:
 
@@ -26,40 +27,54 @@ struct Person {
 
 // Brenna is 19 years old with a salary of $43000 dollars.
 
-// Here is an example of an extraction function:
+// I want to create a Person object with:
+//    firstname = "Brenna"
+//    lastname = "Hanson"
+//    age = 19
+//    salary = 43000
 
+// To do this, I would utilize something like the function below.
+
+// First, we specify our return type as a vector filled with People. We should take in a fileName to read data from.
 vector<Person*> extractData(string fileName) {
     
-    //Lets create an ifstream object:
+    //Lets then create an ifstream object:
     ifstream fileReader(fileName);
     //Create a vector to store all your objects
     vector<Person*> personList;
 
+    // Check to make sure the file is open.
     if (!fileReader.is_open()) {
         cout << "ERR: File is not open.\n";
+        // If it's not open, we return an empty vector
         return personList;
     }
 
+    // Define all the variables a Person may have beforehand.
     string fname, lname;
     int age, sal;
-    while (fileReader >> fname) {
-        //Create your object
+    
+    // Then, we write our while loop with the ifstream object
+    
+    while (fileReader >> fname) { // In this while loop, we are saying:
+        //Create your object      // "Keep putting information into our fname string until there are no more strings left in the file")
         Person* p;
         
-        //Input data into your variables
+        // Input data into your variables in order:
         fileReader >> lname;
         fileReader >> age;
         fileReader >> sal;
 
-        //Then put your data into the object
+        // Then put your data into the object:
         p->firstname = fname;
         p->lastname = lname;
         p->age = age;
         p->salary = sal;
         
-        //Add the person to the vector
+        // Add the person to the vector:
         personList.push_back(p);
-    }
+        
+    } // Then we loop again until the file is empty.
 
     // Make sure to close your file.
     fileReader.close();
